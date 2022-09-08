380c7e8ee97b6f355dd7d28a0c31353ecc878903
Hazal Nur Akyuz <hazalnurakyuz@outlook.com>
2022-09-08 12:14:27 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // str.lenght() => girilen karakterin sayısını döndürür
+        // str1.concat(str2) => string birleştirme
+        // str.indexOf('K') => karakterin konumunu verir
+        // str.charAt(5) => indexOf metoduna benzer şekilde bu sefer de konumu verilen karakteri okur
+        // str.compareTo("Kodlama") => büyük/küçük fark duyarlı olarak karşılaştırır
+        // str..compareToIgnoreCase("KODlama") => büyük/küçük harf duyarlı olmadan karşılaştırır
+        // str.contains("kod") => girilen değer string içinde geçiyor mu diye kontrol eder
+        // str.endsWith("a") => belirtilen karakter(ler) ile bitiyorsa true değerini döndürür
+        // str.replace("Kodlama" , "Vakti") => string değiştirme için kullanılır
+        // str.toLowerCase() , str.toUpperCase();
     }
 }
